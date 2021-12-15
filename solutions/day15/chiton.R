f9654906d2e0dc89548aa0b6f39101ad7a2847be
Riinu <riinuots@gmail.com>
2021-12-15 15:19:10 +0000

    Day 15 edit

--- solutions/day15/chiton.R
+++ solutions/day15/chiton.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(igraph)
-n = read_lines("solutions/day15/input_test", n_max = 1)
+n = read_lines("solutions/day15/input", n_max = 1)
 str_count(n)
 input_orig = read.fwf("solutions/day15/input", rep(1, 100))
 
@@ -34,7 +34,8 @@ cave = cave_joined %>%
   graph_from_data_frame()
 
 
-distances(cave, "1-1", "100-100", mode = "in")
+distances(cave, "1-1", "100-100", mode = "out")
+# distances(cave, "1-1", "10-10", mode = "out")
 
 # Testing, plotting, etc desperation
 
@@ -42,7 +43,8 @@ p = cave_joined %>%
   separate(id_nb, into = c("x", "y"), sep = "-", convert = TRUE) %>% 
   distinct(x, y, weight) %>% 
   ggplot(aes(x, y, label = weight)) +
-  geom_point(size = 0.5, alpha = 0.2) +
+  #geom_point(size = 0.5, alpha = 0.2) +
+  geom_text() +
   theme_void()
 
 #p + coord_cartesian(xlim = c(90, 100), ylim = c(90, 100))
@@ -59,12 +61,7 @@ cave_test = cave_joined %>%
 plot(cave_test)
 #plot(cave)
 
-distances(cave, "1-1", "100-100", mode = "in")
-# 355 too low
-# 369 too high, 368 too high
-# 362 not right
-
-path = shortest_paths(cave, "1-1", "100-100", output = "both")
+path = shortest_paths(cave, "1-1", "10-10", output = "both")
 goto = path$vpath[[1]] %>% as_ids()
 goto = goto[-1]
 
@@ -76,8 +73,8 @@ mypath = tibble(id = goto) %>%
   separate(id, into = c("x", "y"), sep = "-", convert = TRUE) %>% 
   distinct()
 
-p1 = p + geom_point(data = mypath, colour = "orange", alpha = 0.4)
-p1 + scale_y_reverse()
+p1 = p + geom_point(data = mypath, colour = "orange", alpha = 0.6, size = 3)
+p1
 #p1 + coord_cartesian(xlim = c(0, 10), ylim = c(0, 10))
 
 # distances() alternative for result:
